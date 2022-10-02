9a119d767e0c95edfdd31ced826842cb1d5020ca
Kevin Silva Araujo <123kevindante@gmail.com>
2022-10-01 21:45:29 -0300

    portofolio

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $('#btn1').on('click',()=>{
+        $('.paragh').toggle('slow')
+    })
+    $('#btn2',).on('click',() =>{
+        $('#div2').fadeToggle('slow')
+    })
+    $('#btn3').on('click',() =>{
+        $('#div3').slideToggle('slow')
+    })
+    
+})
